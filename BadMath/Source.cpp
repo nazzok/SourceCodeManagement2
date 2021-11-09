02123587a5482e72f0ae1cc28efdd972d39a6335
nazzok <65244920+nazzok@users.noreply.github.com>
2021-11-09 14:02:16 -0500

    Added parenthesis around the ints in average method. Fixed "Third number" prompt to cin to variable n3. Formatted average output.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
@@ -13,11 +13,11 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(1) << fixed << "The average is " << setprecision(2) << fixed << showpoint << a << endl;
 
 	return 0;
 }
